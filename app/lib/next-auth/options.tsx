1fae9f09226b815339b4f0493c3a71dbf7abd7bb
shingoooooon <skksb75@gmail.com>
2024-11-29 17:14:37 +0900

    first migration

---
+++ app/lib/next-auth/options.tsx
@@ -0,0 +1,32 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import prisma from "../prisma"
+
+export const nextAuthOptions: NextAuthOptions = {
+    debug: false,
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+          }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        })
+    ],
+    adapter: PrismaAdapter(prisma),
+    callbacks: {
+        session: ({ session, user }) => {
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    id: user.id
+                }
+            }
+        }
+    }
+
+}
