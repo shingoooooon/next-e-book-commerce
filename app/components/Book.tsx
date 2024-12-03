b563e2d6fd5ae44c405ec7f9ff1dff6038f82f09
shingoooooon <skksb75@gmail.com>
2024-12-03 20:49:39 +0900

    add microcms setting

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -28,7 +28,7 @@ const Book = ({ book }: any) => {
         <a className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
           <Image
             priority
-            src={book.thumbnail}
+            src={book.thumbnail.url}
             alt={book.title}
             width={450}
             height={350}
@@ -37,7 +37,7 @@ const Book = ({ book }: any) => {
           <div className="px-4 py-4 bg-slate-100 rounded-b-md">
             <h2 className="text-lg font-semibold">{book.title}</h2>
             <p className="mt-2 text-lg text-slate-600">この本は○○...</p>
-            <p className="mt-2 text-md text-slate-700">値段：{book.price}円
+            <p className="mt-2 text-md text-slate-700">{book.price}円
             </p>
           </div>
         </a>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-"use client";
+// "use client";
 
 import Book from "./components/Book";
+import { getAllBooks } from "./lib/microcms/clients"
 
 // 疑似データ
 const books = [
@@ -53,14 +54,17 @@ const books = [
 ];
 
 // eslint-disable-next-line @next/next/no-async-client-component
-export default function Home() {
+export default async function Home() {
+  const { contents } = await getAllBooks();
+  console.log(contents)
+
   return (
     <>
       <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
         <h2 className="text-center w-full font-bold text-3xl mb-2">
           Book Commerce
         </h2>
-        {books.map((book) => (
+        {contents.map((book) => (
           <Book key={book.id} book={book} />
         ))}
       </main>
