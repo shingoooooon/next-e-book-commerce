f444f40c38b087f6043604ac6d157640e073d2ff
shingoooooon <skksb75@gmail.com>
2024-12-14 10:18:10 +0900

    modify params type to any temporally

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -3,7 +3,7 @@ import prisma from "../../../lib/prisma";
 
 export async function GET(
     request: Request,
-    { params }: { params: { userId: string } }
+    { params }: any
   ): Promise<NextResponse> {
     const { userId } = await params;
   
